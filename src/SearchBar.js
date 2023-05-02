d8f879609461909e0f52898d793d2ac1b47cefdd
melbesnard85 <melbesnard85@gmail.com>
2023-05-02 10:52:37 +0900

    style: changed header style

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,8 +5,8 @@ function SearchBar({ username, setUsername }) {
     setUsername(e.target.value);
   };
   return (
-    <div className="searchbar mb-4">
-      <label className="block text-gray-700 font-bold mb-2">User Name : </label>
+    <div className="searchbar max-w-md mx-auto mb-4 mt-20">
+      <label className="block text-gray-700 mb-8 text-2xl font-bold text-center">Fetch Github User </label>
       <input
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         placeholder="Search"
