c0ef6df9cee233fa1a729ffcb47834ca348b4e0b
melbesnard85 <melbesnard85@gmail.com>
2023-05-02 11:11:28 +0900

    fix: check empty userData

--- src/UserInfoCard.js
+++ src/UserInfoCard.js
@@ -2,27 +2,29 @@ import React from "react";
 
 function UserInfoCard({ userData }) {
   return (
-    <div className="max-w-md mx-auto rounded shadow-md overflow-hidden">
-      <div className="relative pb-48 overflow-hidden">
-        <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src={userData.avatar_url}
-          alt={userData.login}
-        />
+    (Object.keys(userData).length > 0) ? (
+      <div className="max-w-md mx-auto rounded shadow-md overflow-hidden">
+        <div className="relative pb-48 overflow-hidden">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src={userData.avatar_url}
+            alt={userData.login}
+          />
+        </div>
+        <div className="p-4">
+          {userData.login ? (
+            <div className="dataitem">
+              Login:
+              {userData.login}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <h2 className="text-lg font-bold">{userData.name}</h2>
+          <p className="text-gray-600">{userData.blog}</p>
+        </div>
       </div>
-      <div className="p-4">
-        {userData.login ? (
-          <div className="dataitem">
-            Login:
-            {userData.login}
-          </div>
-        ) : (
-          <div></div>
-        )}
-        <h2 className="text-lg font-bold">{userData.name}</h2>
-        <p className="text-gray-600">{userData.blog}</p>
-      </div>
-    </div>
+    ) : <div></div>
   );
 }
 
